4ca88a8f99cfd26c6a77091e9cf09b29645ed25f
dezhonger <373440955@qq.com>
2019-04-24 22:18:56 +0800

    add dp

---
+++ codeforces/educational round 63/D.cpp
@@ -0,0 +1,58 @@
+#include <cmath>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+#define rep(i, a, b) for( i = (a); i <= (b); i++)
+#define reps(i, a, b) for( i = (a); i < (b); i++)
+#define pb push_back
+#define ps push
+#define mp make_pair
+#define CLR(x,t) memset(x,t,sizeof x)
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+#define Debug(x) cout<<#x<<"="<<x<<endl;
+
+
+const double euler_r = 0.57721566490153286060651209;
+const double pi = 3.141592653589793238462643383279;
+const double E = 2.7182818284590452353602874713526;
+const int inf = ~0U >> 1;
+const int MOD = int (1e9) + 7;
+const double EPS = 1e-6;
+
+typedef long long LL;
+
+
+const int maxn = 300005;
+int i, n, x;
+LL a[maxn];
+LL dp[maxn][3];
+
+int main() {
+    cin >> n >> x;
+    rep (i, 1, n) cin >> a[i];
+    LL r = 0;
+    rep (i, 1, n) {
+        dp[i][0] = max (a[i], dp[i - 1][0] + a[i]);
+        dp[i][1] = max (a[i] * x, max (dp[i - 1][1] + a[i] * x, dp[i - 1][0] + a[i] * x));
+        dp[i][2] = max (dp[i - 1][1] + a[i], dp[i - 1][2] + a[i]);
+        r = max (r, dp[i][0]);
+        r = max (r, dp[i][1]);
+        r = max (r, dp[i][2]);
+    }
+    cout << r << endl;
+    return 0;
+}
