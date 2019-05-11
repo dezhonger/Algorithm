30a8f0cca7625e1e4200ca83045fd019ba8e30c9
dezhonger <373440955@qq.com>
2019-05-11 20:57:31 +0800

    add c sol

--- codeforces/round558 div2/C.cpp
+++ codeforces/round558 div2/C.cpp
@@ -47,7 +47,51 @@ const double EPS = 1e-6;
 
 typedef long long LL;
 
-int main() {
+int gcd (int a, int b) {
+    return b == 0 ? a : gcd (b, a % b);
+}
 
+//int gcd(int a, int b)
+//{
+//	if (a == 0)
+//		return b;
+//	return gcd(b % a, a);
+//}
+
+
+const int maxn = 1005;
+int n, x[maxn], y[maxn];
+map<pair<int, int>, set<long long>> slope_map;
+
+int main() {
+    cin >> n;
+    rep (i, 1, n) cin >>  x[i] >> y[i];
+    LL res = 0;
+    LL total = 0;
+    rep (i, 1, n - 1) rep (j, i + 1, n) {
+        int x1 = x[i];
+        int y1 = y[i];
+        int x2 = x[j];
+        int y2 = y[j];
+        int a = y1 - y2;
+        int b = x1 - x2;
+
+        int d = gcd (abs(a), abs(b));
+        a /= d ;
+        b /= d ;
+        if (a < 0 || (a == 0 && b < 0)) {
+            a = -a;
+            b = -b;
+        }
+
+        pair<int, int> slope (a, b);
+        LL c = 1LL * a * x1 - 1LL * b * y1;
+        if (!slope_map[slope].count (c)) {
+            total++;
+            slope_map[slope].insert (c);
+            res += total - slope_map[slope].size();
+        }
+    }
+    cout << res << endl;
     return 0;
 }
