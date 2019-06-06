b102eb9898657dc7d1b71e8f59c8afd654652266
dezhonger <373440955@qq.com>
2019-06-06 20:17:01 +0800

    cd game

---
+++ codeforces/educational round 66/A.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+#include <string.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = (a); i <= (b); i++)
+#define reps(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back
+#define ps push
+#define mp make_pair
+#define CLR(x,t) memset(x,t,sizeof x)
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+#define Debug(x) cout<<#x<<"="<<x<<endl;
+
+
+const double euler_r = 0.57721566490153286060651209;
+const double pi = 3.141592653589793238462643383279;
+const double E = 2.7182818284590452353602874713526;
+const int inf = ~0U >> 1;
+const int MOD = int (1e9) + 7;
+const double EPS = 1e-6;
+
+typedef long long LL;
+LL n, k;
+
+int main() {
+    ios::sync_with_stdio (false);
+    cin.tie (0);
+    int t;
+    cin >> t;
+    while (t--) {
+        cin >> n >> k;
+        LL ans = 0;
+        while (n > 0) {
+            if (n % k == 0) {
+                n /= k;
+                ans++;
+            } else {
+                LL x = n - n / k * k;
+                n -= x;
+                ans += x;;
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
