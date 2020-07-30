bbacfd70ec5e6f02c16f51a67db0816c7242ae6c
dezhonger <373440955@qq.com>
2020-07-31 00:33:13 +0800

    add

--- model.cpp
+++ model.cpp
@@ -40,10 +40,12 @@ const double euler_r = 0.57721566490153286060651209;
 const double pi = 3.141592653589793238462643383279;
 const double E = 2.7182818284590452353602874713526;
 const int inf = ~0U >> 1;
-const int MOD = int (1e9) + 7;
+const int mod = int (1e9) + 7;
 const double EPS = 1e-6;
 
 typedef long long LL;
+LL powmod(LL a,LL b) {LL res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+LL gcd(LL a,LL b) { return b?gcd(b,a%b):a;}
 
 int main() {
     ios::sync_with_stdio (false);
